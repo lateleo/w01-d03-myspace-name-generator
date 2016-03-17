723f5655286fa403de2edb1a29359143c0d82fe1
Art Burtch <artburtch@huskers.unl.edu>
2016-03-16 19:08:40 -0500

    broken as hell

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,5 +1,22 @@
 require_relative '../lib/name_generator.rb'
 
 # Define whatever methods you need first
+def affirm_equals(actual, expected)
+  if actual == expected
+    puts "Success! '#{actual}' is correct!"
+  else
+    puts "Failure: '#{actual}' does not match '#{expected}'"
+  end
+end
 
-# Now run your actual tests, making sure to do output as necessary
+# 1st Test
+result = name_generator("Art Burtch")
+affirm_equals(result, "xX_ArT_BuRtCh_Xx")
+
+# 2nd Test
+result = name_generator("Arthur Burtch")
+affirm_equals(result, "xX_ArThUr_BuRtCh_Xx")
+
+# 3rd Test
+result = name_generator("@rthur Burtch")
+affirm_equals(result, "Error: please use only letters, numbers or spaces.")
