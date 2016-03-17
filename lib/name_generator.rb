0ae1d7189d0de5cdc8ed427c16b67cedaf44e1e9
Art Burtch <artburtch@huskers.unl.edu>
2016-03-16 21:44:24 -0500

    as done as it's going to be

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -1,57 +1,9 @@
 # You'll have to create your own method(s) here and define them. Use patterns like
 # you've seen elsewhere.
 
-require 'pry'
-def add_letters(arr)
-  alpha = "a"
-  while !arr.include?("z")
-    arr.push(alpha)
-    alpha = alpha.next
-  end
-end
+#require 'pry'
 
-def add_numbers(arr)
-  num = 0
-  while !arr.include?(9)
-    arr.push(num)
-    num += 1
-  end
-end
-
-def populate_valid_chars(arr)
-  add_letters(arr)
-  add_numbers(arr)
-end
-
-def validate_chars(arr)
-  valid_chars = [" "]
-  populate_valid_chars(valid_chars)
-  arr.each do |char|
-    if !valid_chars.include?(char)
-      return false
-    end
-  end
-  true
-end
-
-def letter_push(count, char, arr)
-  arr.push(char.upcase) if count % 2 == 0
-  arr.push(char) if count % 2 != 0
-end
-
-def populate_studly(arr1,arr2)
-  count = 0
-  arr1.each do |char|
-    if char != "_"
-      letter_push(count, char, arr2)
-      count += 1
-    else
-      arr2.push(char)
-      count = 0
-    end
-  end
-end
-
+# Changes the array so that any elements that were spaces are now underscores.
 def replace_spaces(arr)
   arr.each_index do |index|
     if arr[index] == " "
@@ -60,13 +12,76 @@ def replace_spaces(arr)
   end
 end
 
+# Adds a range of characters to an array between `char` and `limit`.
+def add_chars(arr, char, limit)
+  while !arr.include?(limit)
+    arr.push(char)
+    char = char.next
+  end
+end
+
+# Uses add_chars to populate an array with a-z and 0-9 as strings.
+def populate_valid_chars(arr)
+  add_chars(arr, "a", "z")
+  add_chars(arr, "0", "9")
+end
+
+# Determines if the `subject` has any elements not in `reference`.
+def valid_array?(subject, reference)
+  valid = true
+  subject.each do |char|
+    valid = false if !reference.include?(char)
+  end
+  valid
+end
+
+# Fills valid_chars and then checks `arr` for any invalid characters.
+def validate_chars(arr)
+  valid_chars = [" "]
+  populate_valid_chars(valid_chars)
+  valid_array?(arr, valid_chars)
+end
+
+# Determines whether or not the character's case should be changed
+def should_upcase?(char, count)
+  (char != "_") && (count % 2 == 0)
+end
+
+# Modifies the `char` if necessary, and then pushes it.
+def char_push(count, char, arr)
+  arr.push(char.upcase) if should_upcase?(char, count)
+  arr.push(char) if !should_upcase?(char, count)
+end
+
+# Determines what the new value for `count` should be.
+def new_count (count, char)
+  return count.next if char != "_"
+  0
+end
+
+# Uses char_push and new_count to populate the `target` with
+# elements from the `source` in varying letter case.
+def populate_studly(source,target)
+  count = 0
+  source.each do |char|
+    char_push(count, char, target)
+    count = new_count(count, char)
+  end
+end
+
+# Creates a string with alternating letter case and appropriate bookends.
+def finish_studly(source, target)
+  populate_studly(source, target)
+  target.join.prepend("xX_").concat("_Xx")
+end
+
+# Creates a MySpace username based on `name`.
 def name_generator(name)
   lowercase = name.downcase.chars
   return "Error: please only use letters, numbers or spaces." unless validate_chars(lowercase)
   replace_spaces(lowercase)
   studly = []
-  populate_studly(lowercase, studly)
-  studly.join.prepend("xX_").concat("_Xx")
+  finish_studly(lowercase, studly)
 end
 
-binding.pry
+#binding.pry
--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -3,7 +3,7 @@ require_relative '../lib/name_generator.rb'
 # Define whatever methods you need first
 def affirm_equals(actual, expected)
   if actual == expected
-    puts "Success! '#{actual}' is correct!"
+    puts "."
   else
     puts "Failure: '#{actual}' does not match '#{expected}'"
   end
@@ -18,5 +18,17 @@ result = name_generator("Arthur Burtch")
 affirm_equals(result, "xX_ArThUr_BuRtCh_Xx")
 
 # 3rd Test
-result = name_generator("@rthur Burtch")
-affirm_equals(result, "Error: please use only letters, numbers or spaces.")
+result = name_generator("aRTHUR bURTCH")
+affirm_equals(result, "xX_ArThUr_BuRtCh_Xx")
+
+# 4th Test
+rresult = name_generator("ARthUR BURtch")
+affirm_equals(result, "xX_ArThUr_BuRtCh_Xx")
+
+# 5th Test
+result = name_generator("Arthur 13urtch")
+affirm_equals(result, "xX_ArThUr_13UrTcH_Xx")
+
+# 6th Test
+result = name_generator("@r+#ur 13ur+c#!")
+affirm_equals(result, "Error: please only use letters, numbers or spaces.")
