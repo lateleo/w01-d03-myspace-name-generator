5051df233b5e597172abe7120f390d28e1186e1c
Art Burtch <artburtch@huskers.unl.edu>
2016-03-18 16:32:43 -0500

    replaced validate_chars with regex

--- lib/name_generator.rb
+++ lib/name_generator.rb
@@ -1,31 +1,11 @@
 
 require 'pry'
 
-# Returns `true` if elements in `arr` are all letters,
-# numbers, or spaces, and `false` otherwise.
-def validate_chars(arr)
-  result = true
-  arr.each {|char| result = false if /[^0-9a-z_]/.match(char)}
-  result
-end
-# (Expand to see old code)
-# Returns an upcased `char` if it isn't an underscore
-# and `counter` is even, and an unchanged `char` otherwise.
-# def char_modifier(char, counter)
-#  (char != "_") && (counter % 2 == 0) ? char.upcase : char
-# end
-#
-# Returns `counter[0] + 1` if `char` isn't an underscore,
-# and `1` otherwise.
-#def new_counter(counter, char)
-#  char != "_" ? counter.next : 1
-#end
-
 # Generates a new array with elements mapped from `source`
 # and alternatively capitalized.
-def populate_studly(source)
+def studly(source)
   counter = 1
-  source.map do |char|
+  source.chars.map do |char|
     counter = (char != "_" ? counter.next : 1)
     (char != "_") && (counter % 2 == 0) ? char.upcase : char
   end
@@ -33,9 +13,9 @@ end
 # ------------------------------------------------
 # Creates a MySpace username based on `name`.
 def name_generator(name)
-  lowercase = name.downcase.gsub(/ /,"_").chars
-  return "Error: please only use letters, numbers, spaces or underscores." unless validate_chars(lowercase)
-  populate_studly(lowercase).join.prepend("xX_").concat("_Xx")
+  subs = name.downcase.gsub(/ /,"_")
+  return "Please only use alphanumerics, spaces or underscores." if /[^0-9a-z_]/.match(subs) != nil
+  studly(subs).join.prepend("xX_").concat("_Xx")
 end
 
 #binding.pry
--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -5,47 +5,15 @@ require_relative "test_helper.rb"
 class NameGeneratorTest < Minitest::Test
 
 # add first test soon
-  def test_valid_chars
-    result = validate_chars(["b","c","d","x","y","_","1","2","7","8"])
-    assert(result, "Characters should be valid")
-  end
+  #def test_valid_chars
+  #  result = validate_chars(["b","c","d","x","y","_","1","2","7","8"].join)
+  #  assert(result, "Characters should be valid")
+  #end
 
-  def test_invalid_chars
-    result = validate_chars(["a","b","@"])
-    refute(result, "'@' is an invalid character")
-  end
-
-# (Expand to see old tests)
-#  def test_char_mod_upcase
-#    result = char_modifier("a", 0)
-#    assert_equal("A", result, "Should be capital A")
-#  end
-#
-#  def test_char_mod_no_upcase
-#    result = char_modifier("a", 1)
-#    assert_equal("a", result, "Should be lowercase a")
-#  end
-#
-#  def test_char_mod_number
-#    upcase = char_modifier("5", 0)
-#    no_upcase = char_modifier("5", 1)
-#    assert_equal(upcase, no_upcase, "Should not change")
-#  end
-#
-#  def test_new_counter_a
-#    result = new_counter(0, "a")
-#    assert_equal(1, result, "Should be 1")
-#  end
-#
-#  def test_new_counter_5
-#    result = new_counter(0, "5")
-#    assert_equal(1, result, "Should be 1")
-#  end
-#
-#  def test_new_counter_underscore
-#    result = new_counter(5, "_")
-#    assert_equal(1, result, "Should be 0")
-#  end
+  #def test_invalid_chars
+  #  result = validate_chars(["a","b","@"].join)
+  #  refute(result, "'@' is an invalid character")
+  #end
 
   def test_Art_Burtch
     result = name_generator("Art Burtch")
@@ -64,7 +32,7 @@ class NameGeneratorTest < Minitest::Test
 
   def test_symbols
     result = name_generator("@r+#ur 13ur+c#!")
-    assert_equal("Error: please only use letters, numbers, spaces or underscores.",
+    assert_equal("Error: please only use alphanumerics, spaces or underscores.",
     result, "Should have recieved error message")
   end
 end
