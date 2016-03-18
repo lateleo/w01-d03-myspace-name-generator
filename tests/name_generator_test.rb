cca49f8c46e3ff7c171126236201665969aac83e
Art Burtch <artburtch@huskers.unl.edu>
2016-03-18 16:33:12 -0500

    replaced validate_chars with regex

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -4,17 +4,6 @@ require_relative "test_helper.rb"
 # Define whatever methods you need first
 class NameGeneratorTest < Minitest::Test
 
-# add first test soon
-  #def test_valid_chars
-  #  result = validate_chars(["b","c","d","x","y","_","1","2","7","8"].join)
-  #  assert(result, "Characters should be valid")
-  #end
-
-  #def test_invalid_chars
-  #  result = validate_chars(["a","b","@"].join)
-  #  refute(result, "'@' is an invalid character")
-  #end
-
   def test_Art_Burtch
     result = name_generator("Art Burtch")
     assert_equal("xX_ArT_BuRtCh_Xx", result, "Should be 'xX_ArT_BuRtCh_Xx'")
